bcc39382162f21d6a75025de53d2ceb37ae50b8d
valemorenodev <valentinamoreno2002@gmail.com>
2025-12-25 21:43:53 -0500

    implementar rutas de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/login/login';
+import { Course } from './components/course/course';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Profile } from './components/profile/profile';
+
+const routes: Routes = [
+  // 👉 Inicio en Login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'course', component: Course },
+  { path: 'profile', component: Profile },
+
+  // 👉 Cualquier ruta inválida vuelve a login
+  { path: '**', redirectTo: 'login' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/components/profile/profile.spec.ts
+++ src/app/components/profile/profile.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Porfile } from './porfile';
+import { Profile } from './profile';
 
-describe('Porfile', () => {
-  let component: Porfile;
-  let fixture: ComponentFixture<Porfile>;
+describe('Profile', () => {
+  let component: Profile;
+  let fixture: ComponentFixture<Profile>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Porfile]
+      imports: [Profile]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Porfile);
+    fixture = TestBed.createComponent(Profile);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

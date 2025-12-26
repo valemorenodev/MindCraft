be0229a85c633230d3188accf919c3c706a02f6e
valemorenodev <valentinamoreno2002@gmail.com>
2025-12-26 14:05:15 -0500

    implementar servicio de autenticacion

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+  imports: [FormsModule],
+  templateUrl: './login.html'
 })
-export class Login {
+export class LoginComponent {
+  email = '';
+  password = '';
 
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  submit() {
+    this.auth.login(this.email, this.password).subscribe({
+      next: (res: any) => {
+        this.auth.saveToken(res.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        alert('Credenciales inválidas');
+      }
+    });
+  }
 }
---
+++ src/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/auth/login`, {
+      email,
+      password
+    });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}

afcdbdb8da42ce1fa3889f07d804c6518e9e85e4
valemorenodev <valentinamoreno2002@gmail.com>
2025-12-30 11:38:18 -0500

    Refactor app configuration to include router setup and clean up route definitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+
+import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
     provideHttpClient(
+      withFetch(),  
       withInterceptors([authInterceptor])
     )
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,9 @@
-import {Routes } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
-import { Course } from './components/course/course';
 import { Dashboard } from './components/dashboard/dashboard';
-import { Profile } from './components/profile/profile';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'course', component: Course },
-  { path: 'profile', component: Profile },
-
-  { path: '**', redirectTo: 'login' }
+  { path: 'dashboard', component: Dashboard }
 ];
